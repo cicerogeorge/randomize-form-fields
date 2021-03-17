7f76fae3b39b82a4bc3cc6121ac4b4374cf6fcae
Cicero Monteiro <cicerogeorge@gmail.com>
2021-03-17 14:56:10 -0300

    Main plugin class

---
+++ jquery.randomizeFormFields.js
@@ -0,0 +1,50 @@
+/*
+ * Randomize Form Fields for jQuery
+ *
+ * Copyright (c) 2021 Cicero Monteiro
+ * cicerogeorge@gmail.com
+ *
+ * Date: March 17 2021
+ * Version: 1.0.1
+ *
+ * Randomize form fields names to avoid browser autofill
+ */
+
+(function($) {
+	$.fn.randomizeFormFields = function( options ) {
+		var settings = $.extend({
+			'length': 25
+		}, options);
+
+		var fields = {};
+
+		this.find( 'input' ).each(function(){
+			// check if input has a name
+			const field_name = $( this ).attr( 'name' );
+			if (typeof field_name !== 'undefined' && field_name !== false && field_name !== '') {
+				// randomizes field name
+				const randomName = getRandomName(settings.length);
+				fields[randomName] = $( this ).attr( 'name' );
+				$( this ).attr( 'name', randomName );
+			}
+		});
+
+		this.submit(function( e ){
+			// get fields names back before submit
+			$( this ).find( 'input' ).each(function(){
+				$( this ).attr( 'name', fields[$( this ).attr( 'name' )] );
+			});
+		});
+
+		return this;
+	};
+
+	function getRandomName( length ) {
+		let rand = '';
+		const char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+		for ( let i = 0; i < length; i++ ) {
+			rand += char_list.charAt(Math.floor(Math.random() * char_list.length));
+		}
+		return rand;
+	}
+}( jQuery ));
